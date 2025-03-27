7000399fc27be8151a56aaeaeb07cbd3492050f0
Mikhael Abdallah de Oliveira Pinto <mikhael_abdallah@usp.br>
2025-03-27 19:15:40 -0300

    Implement Move Zeros solution in Go

---
+++ Arrays/move-zeroes/move-zeroes.go
@@ -0,0 +1,15 @@
+package main
+
+func moveZeroes(nums []int)  {
+    zeroCount := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] == 0 {
+            zeroCount++
+        } else {
+            nums[i-zeroCount] = nums[i]
+        }
+    }
+    for i := 0; i < zeroCount; i++ {
+        nums[len(nums)-i-1] = 0
+    }
+}
